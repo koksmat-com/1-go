0106e2f3fa9f09d05fb12095228347158aeddb16
Niels Gregers Johansen <niels@timeboxer.dk>
2024-06-20 07:11:02 +0200

    more

---
+++ 05-sqlsnippets/insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.XX(id, created_at, created_by, updated_at, updated_by, deleted_at, tenant, searchindex, name, url)
+    VALUES (DEFAULT, DEFAULT, '', DEFAULT, '', DEFAULT, '', 'name:value', 'value','');
+
---
+++ test/step-1-2-3.ps1
@@ -0,0 +1,7 @@
+koksmat trace log "step  1"
+Start-Sleep -Seconds 1
+koksmat trace log "step  2"
+Start-Sleep -Seconds 1
+koksmat trace log "step  3"
+Start-Sleep -Seconds 1
+koksmat trace log "done"
